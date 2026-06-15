d395ee8327a8e46f8e5adad91ff70f1503712f8f
mukumakabanda-alt <mukumakabanda@gmail.com>
2026-06-15 08:20:41 +0200

    Delete src/routes/admin.login.tsx

--- src/routes/admin.login.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/admin/login")({
-  beforeLoad: () => {
-    throw redirect({ to: "/admin" });
-  },
-  component: () => null,
-});
